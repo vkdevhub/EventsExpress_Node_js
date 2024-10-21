41b8b6223ad584a8fe47a8543784bf87b4db4e9e
vkdevhub <vmkoptiev@gmail.com>
2024-10-21 17:50:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const { startApp } = require("./databaseSetup");
